7e4095b959f09d4342d72c81ce333a64e6effe07
Michael Squires <sqmk@php.net>
2013-04-18 15:51:33 -0700

    GitHub #13 - Add retry, expire, callback getters on EmergencyPriority

--- library/Pushy/Priority/EmergencyPriority.php
+++ library/Pushy/Priority/EmergencyPriority.php
@@ -18,4 +18,58 @@ class EmergencyPriority extends AbstractPriority
      * Priority code
      */
     const CODE = 2;
+
+    /**
+     * Retry period in seconds
+     *
+     * @var int
+     */
+    protected $retry = 30;
+
+    /**
+     * Expires in seconds
+     *
+     * @var int
+     */
+    protected $expire = 86400;
+
+    /**
+     * Set retry
+     *
+     * @param int $seconds Seconds
+     *
+     * @return self This object
+     */
+    public function setRetry($seconds)
+    {
+        $this->retry = (int) $seconds;
+
+        return $this;
+    }
+
+    /**
+     * Set expire
+     *
+     * @param int $seconds Seconds
+     *
+     * @return self This object
+     */
+    public function setExpire($seconds)
+    {
+        $this->expire = (int) $seconds;
+
+        return $this;
+    }
+
+    /**
+     * Set callback URL
+     *
+     * @param string $url Callback URL
+     */
+    public function setCallback($url)
+    {
+        $this->expire = (string) $url;
+
+        return $this;
+    }
 }
