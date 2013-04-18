7b85ddb63bd5eb6d47bd97f038e80406e0810f0a
Michael Squires <sqmk@php.net>
2013-04-18 02:09:56 -0400

    GitHub #13 - Add low, normal, high, emergency priority

---
+++ library/Pushy/Priority/HighPriority.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Pushy: Pushover PHP Client
+ *
+ * @author    Michael Squires <sqmk@php.net>
+ * @copyright Copyright (c) 2013 Michael K. Squires
+ * @license   http://github.com/sqmk/Pushy/wiki/License
+ */
+
+namespace Pushy\Priority;
+
+/**
+ * High Priority
+ */
+class HighPriority extends AbstractPriority
+{
+    /**
+     * Priority code
+     */
+    const CODE = 1;
+}
---
+++ library/Pushy/Priority/LowPriority.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Pushy: Pushover PHP Client
+ *
+ * @author    Michael Squires <sqmk@php.net>
+ * @copyright Copyright (c) 2013 Michael K. Squires
+ * @license   http://github.com/sqmk/Pushy/wiki/License
+ */
+
+namespace Pushy\Priority;
+
+/**
+ * Low Priority
+ */
+class LowPriority extends AbstractPriority
+{
+    /**
+     * Priority code
+     */
+    const CODE = -1;
+}
