6efae2a4ffc27be01385be38531ba24ccbac7a46
Michael Squires <sqmk@php.net>
2013-04-20 01:13:16 -0400

    GitHub #13 - Add test for AbstractPriority

---
+++ tests/PushyTest/Priority/AbstractPriorityTest.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Pushy: Pushover PHP Client
+ *
+ * @author    Michael Squires <sqmk@php.net>
+ * @copyright Copyright (c) 2013 Michael K. Squires
+ * @license   http://github.com/sqmk/Pushy/wiki/License
+ */
+
+namespace PushyTest\Priority;
+
+/**
+ * Tests for Pushy\Priority\AbstractPriority
+ */
+class AbstractPriorityTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Priority
+     *
+     * @var AbstractPriority
+     */
+    protected $priority;
+
+    /**
+     * Set up
+     */
+    public function setUp()
+    {
+        $this->priority = $this->getMockForAbstractClass(
+            '\Pushy\Priority\AbstractPriority'
+        );
+    }
+
+    /**
+     * Test: Get code
+     *
+     * @covers Pushy\Priority\AbstractPriority::getCode
+     */
+    public function testGetCode()
+    {
+        // Reference locally to access constants
+        $priority = $this->priority;
+
+        $this->assertEquals($this->priority->getCode(), $priority::CODE);
+    }
+}
