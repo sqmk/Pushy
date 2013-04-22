2cc6e5582c7730b7310ec9067df4dda49d2fbdb3
Michael Squires <sqmk@php.net>
2013-04-21 22:15:50 -0400

    GitHub #8 - Add tests for Message

---
+++ tests/PushyTest/MessageTest.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * Pushy: Pushover PHP Client
+ *
+ * @author    Michael Squires <sqmk@php.net>
+ * @copyright Copyright (c) 2013 Michael K. Squires
+ * @license   http://github.com/sqmk/Pushy/wiki/License
+ */
+
+namespace PushyTest;
+
+use Pushy\Message;
+
+/**
+ * Tests for Pushy\Message
+ */
+class MessageTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Message
+     *
+     * @var Message
+     */
+    protected $message;
+
+    /**
+     * Mock user
+     *
+     * @var \Pushy\User
+     */
+    protected $mockUser;
+
+    /**
+     * Mock priority
+     *
+     * @var \Pushy\Priority\PriorityInterface
+     */
+    protected $mockPriority;
+
+    /**
+     * Mock sound
+     *
+     * @var \Pushy\Sound\SoundInterface
+     */
+    protected $mockSound;
+
+    /**
+     * Set up
+     *
+     * @covers Pushy\Message::__construct
+     */
+    public function setUp()
+    {
+        $this->message = new Message;
+
+        // Set mocks
+        $this->mockUser     = $this->getMock('\Pushy\User');
+        $this->mockPriority = $this->getMock('\Pushy\Priority\PriorityInterface');
+        $this->mockSound    = $this->getMock('\Pushy\Sound\SoundInterface');
+    }
+
+    /**
+     * Test: Constructor defaults
+     *
+     * @covers Pushy\Message::__construct
+     */
+    public function testConstructorDefaults()
+    {
+        // Ensure a priority is automatically set
+        $this->assertAttributeInstanceOf(
+            '\Pushy\Priority\PriorityInterface', 'priority', $this->message
+        );
+
+        // Ensure a sound is automatically set
+        $this->assertAttributeInstanceOf(
+            '\Pushy\Sound\SoundInterface', 'sound', $this->message
+        );
+    }
+
+    /**
+     * Test: Set user
+     *
+     * @covers Pushy\Message::setUser
+     */
+    public function testSetUser()
+    {
+        $this->message->setUser($this->mockUser);
+
+        $this->assertAttributeInstanceOf(
+            '\Pushy\Sound\SoundInterface', 'sound', $this->message
+        );
+    }
+
+    /**
+     * Test: Set priority
+     *
+     * @covers Pushy\Message::setPriority
+     */
+    public function testSetPriority()
+    {
+        $this->message->setPriority($this->mockPriority);
+
+        $this->assertAttributeInstanceOf(
+            '\Pushy\Priority\PriorityInterface', 'priority', $this->message
+        );
+    }
+
+    /**
+     * Test: Set sound
+     *
+     * @covers Pushy\Message::setSound
+     */
+    public function testSetSound()
+    {
+        $this->message->setSound($this->mockSound);
+
+        $this->assertAttributeInstanceOf(
+            '\Pushy\Sound\SoundInterface', 'sound', $this->message
+        );
+    }
+}
