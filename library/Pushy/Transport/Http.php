9cac378c01b0e6ecbcc3675ca43810887504b7a8
Michael Squires <sqmk@php.net>
2013-04-24 16:04:23 -0700

    GitHub #12 - Change sendRequest to accept RequestMessage rather than constructor

--- library/Pushy/Transport/Http.php
+++ library/Pushy/Transport/Http.php
@@ -15,38 +15,21 @@ namespace Pushy\Transport;
 class Http implements TransportInterface
 {
     /**
-     * Request message
-     *
-     * @var RequestMessage
-     */
-    protected $requestMessage;
-
-    /**
-     * Instantiates HTTP client
-     *
-     * @param RequestMessage $requestMessage Request message
-     */
-    public function __construct(RequestMessage $requestMessage)
-    {
-        $this->requestMessage = $requestMessage;
-    }
-
-    /**
      * Send request
      *
      * @return array Response data
      */
-    public function sendRequest()
+    public function sendRequest(RequestMessage $requestMessage)
     {
         // Initialize curl and set options
         $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, $this->requestMessage->getFullUrl());
-        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $this->requestMessage->getMethod());
+        curl_setopt($curl, CURLOPT_URL, $requestMessage->getFullUrl());
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $requestMessage->getMethod());
         curl_setopt($curl, CURLOPT_HEADER, false);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
         // Set JSON body if available
-        if ($body = $this->requestMessage->getJsonBody()) {
+        if ($body = $requestMessage->getJsonBody()) {
             curl_setopt($curl, CURLOPT_POSTFIELDS, $body);
         }
 
