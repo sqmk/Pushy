593ee57ca8bc24c8b3be0acfd7b087311072f2c8
Michael Squires <sqmk@php.net>
2013-04-27 20:34:53 -0400

    GitHub #12 - Add ConnectionException

---
+++ library/Pushy/Transport/Exception/ConnectionException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Pushy: Pushover PHP Client
+ *
+ * @author    Michael Squires <sqmk@php.net>
+ * @copyright Copyright (c) 2013 Michael K. Squires
+ * @license   http://github.com/sqmk/Pushy/wiki/License
+ */
+
+namespace Pushy\Transport\Exception;
+
+/**
+ * Connection error
+ */
+class ConnectionException extends \Exception {}
