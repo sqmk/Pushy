df31f1af6bab94bcd81145a11cfcb75730a604b5
Michael Squires <sqmk@php.net>
2013-05-01 22:45:38 -0400

    GitHub #3 - Fix broken test

--- tests/PushyTest/ClientTest.php
+++ tests/PushyTest/ClientTest.php
@@ -38,16 +38,48 @@ class ClientTest extends \PHPUnit_Framework_TestCase
     protected $mockUser;
 
     /**
+     * Mock priority
+     *
+     * @var \Pushy\Priority\PriorityInterface
+     */
+    protected $mockPriority;
+
+    /**
+     * Mock transport
+     *
+     * @var \Pushy\Transport\TransportInterface
+     */
+    protected $mockTransport;
+
+    /**
      * Set up
      *
      * @covers Pushy\Client::__construct
      */
     public function setUp()
     {
+        // Set up mocks
         $this->mockMessage   = $this->getMock('\Pushy\Message');
         $this->mockUser      = $this->getMock('\Pushy\User');
+        $this->mockPriority  = $this->getMock('\Pushy\Priority\PriorityInterface');
         $this->mockTransport = $this->getMock('\Pushy\Transport\TransportInterface');
 
+        // Mock priority needs list of api params
+        $this->mockPriority->expects($this->any())
+            ->method('getApiParameters')
+            ->will($this->returnValue([]));
+
+        // Mock message needs user
+        $this->mockMessage->expects($this->any())
+            ->method('getUser')
+            ->will($this->returnValue($this->mockUser));
+
+        // Mock message needs priority
+        $this->mockMessage->expects($this->any())
+            ->method('getPriority')
+            ->will($this->returnValue($this->mockPriority));
+
+        // Set up client
         $this->client = new Client('apikey123');
         $this->client->setTransport($this->mockTransport);
     }
