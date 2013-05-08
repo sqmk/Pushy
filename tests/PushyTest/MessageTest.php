85b3913ec30b1b80c54c6c7d7a24465b8132d637
Michael Squires <sqmk@php.net>
2013-05-08 01:33:15 -0400

    GitHub #3 - Add additional tests for messages for full coverage

--- tests/PushyTest/MessageTest.php
+++ tests/PushyTest/MessageTest.php
@@ -78,108 +78,188 @@ class MessageTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * Test: Set user
+     * Test: Get/Set user
      *
+     * @covers Pushy\Message::getUser
      * @covers Pushy\Message::setUser
      */
-    public function testSetUser()
+    public function testGetSetUser()
     {
         $this->message->setUser($this->mockUser);
 
-        $this->assertAttributeInstanceOf(
-            '\Pushy\Sound\SoundInterface', 'sound', $this->message
+        $this->assertEquals(
+            $this->message->getUser(),
+            $this->mockUser
         );
     }
 
     /**
-     * Test: Set priority
+     * Test: Get/Set message
      *
-     * @covers Pushy\Message::setPriority
-     */
-    public function testSetPriority()
-    {
-        $this->message->setPriority($this->mockPriority);
-
-        $this->assertAttributeInstanceOf(
-            '\Pushy\Priority\PriorityInterface', 'priority', $this->message
-        );
-    }
-
-    /**
-     * Test: Set sound
-     *
-     * @covers Pushy\Message::setSound
-     */
-    public function testSetSound()
-    {
-        $this->message->setSound($this->mockSound);
-
-        $this->assertAttributeInstanceOf(
-            '\Pushy\Sound\SoundInterface', 'sound', $this->message
-        );
-    }
-
-    /**
-     * Test: Set message
-     *
+     * @covers Pushy\Message::getMessage
      * @covers Pushy\Message::setMessage
      */
-    public function testSetMessage()
+    public function testGetSetMessage()
     {
         $message = 'PHPUnit Test Message';
 
         $this->message->setMessage($message);
 
-        $this->assertAttributeEquals(
-            $message, 'message', $this->message
+        $this->assertEquals(
+            $this->message->getMessage(),
+            $message
         );
     }
 
     /**
-     * Test: Set title
+     * Test: Set invalid length message
      *
+     * @covers Pushy\Message::setMessage
+     *
+     * @expectedException \InvalidArgumentException
+     */
+    public function testSetMessageWithInvalidLength()
+    {
+        $this->message->setMessage(str_repeat('X', 513));
+    }
+
+    /**
+     * Test: Get/Set title
+     *
+     * @covers Pushy\Message::getTitle
      * @covers Pushy\Message::setTitle
      */
-    public function testSetTitle()
+    public function testGetSetTitle()
     {
         $title = 'PHPUnit Test Title';
 
         $this->message->setTitle($title);
 
-        $this->assertAttributeEquals(
-            $title, 'title', $this->message
+        $this->assertEquals(
+            $this->message->getTitle(),
+            $title
         );
     }
 
     /**
-     * Test: Set URL
+     * Test: Set invalid length title
      *
+     * @covers Pushy\Message::setTitle
+     *
+     * @expectedException \InvalidArgumentException
+     */
+    public function testSetTitleWithInvalidLength()
+    {
+        $this->message->setTitle(str_repeat('X', 101));
+    }
+
+    /**
+     * Test: Get/Set URL
+     *
+     * @covers Pushy\Message::getUrl
      * @covers Pushy\Message::setUrl
      */
-    public function testSetUrl()
+    public function testGetSetUrl()
     {
         $url = 'http://example.com';
 
         $this->message->setUrl($url);
 
-        $this->assertAttributeEquals(
-            $url, 'url', $this->message
+        $this->assertEquals(
+            $this->message->getUrl(),
+            $url
         );
     }
 
     /**
-     * Test: Set URL title
+     * Test: Set invalid length URL
      *
+     * @covers Pushy\Message::setUrl
+     *
+     * @expectedException \InvalidArgumentException
+     */
+    public function testSetUrlWithInvalidLength()
+    {
+        $this->message->setUrl(str_repeat('X', 501));
+    }
+
+    /**
+     * Test: Get/Set URL title
+     *
+     * @covers Pushy\Message::getUrlTitle
      * @covers Pushy\Message::setUrlTitle
      */
-    public function testSetUrlTitle()
+    public function testGetSetUrlTitle()
     {
         $urlTitle = 'PHPUnit Test URL Title';
 
         $this->message->setUrlTitle($urlTitle);
 
-        $this->assertAttributeEquals(
-            $urlTitle, 'urlTitle', $this->message
+        $this->assertEquals(
+            $this->message->getUrlTitle(),
+            $urlTitle
+        );
+    }
+
+    /**
+     * Test: Set invalid length URL title
+     *
+     * @covers Pushy\Message::setUrlTitle
+     *
+     * @expectedException \InvalidArgumentException
+     */
+    public function testSetUrlTitleWithInvalidLength()
+    {
+        $this->message->setUrlTitle(str_repeat('X', 51));
+    }
+
+    /**
+     * Test: Get/Set priority
+     *
+     * @covers Pushy\Message::getPriority
+     * @covers Pushy\Message::setPriority
+     */
+    public function testGetSetPriority()
+    {
+        $this->message->setPriority($this->mockPriority);
+
+        $this->assertEquals(
+            $this->message->getPriority(),
+            $this->mockPriority
+        );
+    }
+
+    /**
+     * Test: Get/Set timestamp
+     *
+     * @covers Pushy\Message::getTimestamp
+     * @covers Pushy\Message::setTimestamp
+     */
+    public function testGetSetTimestamp()
+    {
+        $time = time();
+
+        $this->message->setTimestamp($time);
+
+        $this->assertEquals(
+            $this->message->getTimestamp(),
+            $time
+        );
+    }
+
+    /**
+     * Test: Get/Set sound
+     *
+     * @covers Pushy\Message::getSound
+     * @covers Pushy\Message::setSound
+     */
+    public function testGetSetSound()
+    {
+        $this->message->setSound($this->mockSound);
+
+        $this->assertEquals(
+            $this->message->getSound(),
+            $this->mockSound
         );
     }
 }
