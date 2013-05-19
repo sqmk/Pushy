eb1ebed53351c6060fd2eb0eae1fc7f9549958ff
Michael Squires <sqmk@php.net>
2013-05-19 01:13:13 -0400

    GitHub #20 - Add tests for Http for full coverage

---
+++ tests/PushyTest/Transport/HttpTest.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Pushy: Pushover PHP Client
+ *
+ * @author    Michael Squires <sqmk@php.net>
+ * @copyright Copyright (c) 2013 Michael K. Squires
+ * @license   http://github.com/sqmk/Pushy/wiki/License
+ */
+
+namespace PushyTest\Transport\Http;
+
+use Pushy\Transport\Http;
+
+/**
+ * Tests for Pushy\Transport\Http
+ */
+class HttpTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Http instance
+     *
+     * @var Http
+     */
+    protected $transportHttp;
+
+    /**
+     * Mock request message
+     *
+     * @var \Pushy\Transport\RequestMessage
+     */
+    protected $mockRequestMessage;
+
+    /**
+     * Set up
+     */
+    public function setUp()
+    {
+        $this->transportHttp = new Http;
+
+        // Create mock request message
+        $this->mockRequestMessage = $this->getMock(
+            '\Pushy\Transport\RequestMessage'
+        );
+    }
+
+    /**
+     * Test: Send request
+     *
+     * @covers \Pushy\Transport\Http::sendRequest
+     *
+     * @expectedException \Pushy\Transport\Exception\ConnectionException
+     */
+    public function testSend()
+    {
+        $this->transportHttp->sendRequest($this->mockRequestMessage);
+    }
+}
