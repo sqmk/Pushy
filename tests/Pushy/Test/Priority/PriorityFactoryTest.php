76a66fc5106e72c085d1c960d979c6d4fba37909
Michael Squires <sqmk@php.net>
2013-11-01 23:15:43 -0700

    GitHub #38 - Add test for Priority Factory

---
+++ tests/Pushy/Test/Priority/PriorityFactoryTest.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Pushy: Pushover PHP Client
+ *
+ * @author    Michael Squires <sqmk@php.net>
+ * @copyright Copyright (c) 2013 Michael K. Squires
+ * @license   http://github.com/sqmk/Pushy/wiki/License
+ */
+
+namespace Pushy\Test\Priority;
+
+use Pushy\Priority\PriorityFactory;
+
+/**
+ * Tests for Pushy\Priority\PriorityFactory
+ */
+class PriorityFactoryTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test: Create name
+     *
+     * @covers \Pushy\Priority\PriorityFactory::createPriority
+     */
+    public function testCreatePriority()
+    {
+        $this->assertInstanceOf(
+            '\Pushy\Priority\NormalPriority',
+            PriorityFactory::createPriority('normal')
+        );
+    }
+
+    /**
+     * Test: Create name, invalid priority
+     *
+     * @covers \Pushy\Priority\PriorityFactory::createPriority
+     *
+     * @expectedException \Exception
+     */
+    public function testCreatePriorityInvalidPriority()
+    {
+        PriorityFactory::createPriority('this_doesnt_exist');
+    }
+}
