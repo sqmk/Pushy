50bbe21aeafe882e33cfeadd70d79c619c19996f
Michael K. Squires <sqmk@php.net>
2014-10-14 20:11:54 -0700

    GitHub #60 - Add lowest priority class

---
+++ library/Pushy/Priority/LowestPriority.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Pushy: Pushover PHP Client
+ *
+ * @author    Michael Squires <sqmk@php.net>
+ * @copyright Copyright (c) 2013-2014 Michael K. Squires
+ * @license   http://github.com/sqmk/Pushy/wiki/License
+ */
+
+namespace Pushy\Priority;
+
+/**
+ * Lowest Priority
+ */
+class LowestPriority extends AbstractPriority
+{
+    /**
+     * Priority code
+     */
+    const CODE = -2;
+}
